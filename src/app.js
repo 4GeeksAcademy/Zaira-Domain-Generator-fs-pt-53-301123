273f5ff5d3efef03282eb55fa9eed56c565a1870
Zaira <135705060+gilzaira@users.noreply.github.com>
2023-11-30 18:31:48 +0000

    first commit domain generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domainGenerator").innerHTML = generateDomain();
 };
+function generateDomain() {
+  let pronouns = ["i", "you", "the", "we", "one", "our"];
+  let adjectives = ["cool", "newbie", "ingenious", "bold", "sharp", "absurd"];
+  let nouns = ["cat", "developer", "person", "spirit", "brain", "senior"];
+  let extensions = [".com", ".tech", ".pro", ".zed", ".info", ".web"];
+
+  let domain = "";
+
+  let domainParts = [pronouns, adjectives, nouns, extensions];
+
+  for (let wholeDomain of domainParts) {
+    let randomIndex = Math.floor(Math.random() * wholeDomain.length);
+    domain += wholeDomain[randomIndex];
+  }
+  return domain;
+}
+
+console.log(generateDomain());
