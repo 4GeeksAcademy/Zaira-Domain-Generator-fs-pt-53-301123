58cd85e768bf616eb6db4b26a468e204236788ae
Zaira <135705060+gilzaira@users.noreply.github.com>
2023-12-03 18:43:53 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -6,23 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.querySelector("#domainGenerator").innerHTML = generateDomain();
-};
-function generateDomain() {
-  let pronouns = ["i", "you", "the", "we", "one", "our"];
-  let adjectives = ["cool", "newbie", "ingenious", "bold", "sharp", "absurd"];
-  let nouns = ["cat", "developer", "person", "spirit", "brain", "senior"];
-  let extensions = [".com", ".tech", ".pro", ".zed", ".info", ".web"];
+  let pronouns = ["the", "one"];
+  let adjectives = ["cool", "newbie", "bold", "absurd"];
+  let nouns = ["developer", "spirit", "brain"];
+  let extensions = [".tech", ".pro"];
 
-  let domain = "";
+  let allArrays = [pronouns, adjectives, nouns, extensions];
+  let domains = allArrays.reduce(function totalCombinations(total, arr) {
+    return total * arr.length;
+  }, 1);
 
-  let domainParts = [pronouns, adjectives, nouns, extensions];
+  let allDomains = [];
 
-  for (let wholeDomain of domainParts) {
-    let randomIndex = Math.floor(Math.random() * wholeDomain.length);
-    domain += wholeDomain[randomIndex];
+  for (let i = 0; i < domains; i++) {
+    let innerIndex = i;
+    let combined = allArrays
+      .map(function totalCombinations(arr) {
+        let index = innerIndex % arr.length;
+        innerIndex = Math.floor(i / arr.length);
+        return arr[index];
+      })
+      .join("");
+
+    allDomains.push(combined);
   }
-  return domain;
-}
-
-console.log(generateDomain());
+  document.querySelector("#domainGenerator").innerHTML = allDomains.join(
+    "<br>"
+  );
+};
